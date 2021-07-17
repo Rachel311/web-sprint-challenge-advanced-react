3b0e2b25f3c693b62d4c4a9ce58ebbc40a5f9660
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-07-17 14:12:24 -0600

    completed useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,20 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+const useForm = (key, initialValue) => {
+    const [state, setState] = useState(key, initialValue);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    const handleChanges = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
+
+    return [state, handleChanges, handleSubmit, showSuccessMessage]
+}
+
+export default useForm
